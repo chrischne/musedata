853751783ca3663e7e9dd43dbbdd183b4c30a8bb
chrischne <chrischne@gmail.com>
2016-10-16 16:29:59 +0200

    changing museDate architecture

--- dev/sketch.js
+++ dev/sketch.js
@@ -7,7 +7,10 @@ muse = museData().dummyData();
 //muse = museData().dummyData(500);
 
 //setting up callbacks to specific id's
-muse.listenTo('/muse/eeg',parseMsg);
+//muse.listenTo('/muse/eeg',parseMsg);
+muse.listenTo('/muse/eeg');
+muse.listenTo('/muse//muse/elements/raw_fft0');
+muse.listenTO('/muse/elements/experimental/mellow');
 //muse.listenTo('/muse/elements/raw_fft0', parseMsg);
 
 //start data transmission
@@ -20,7 +23,25 @@ function setup() {
 
 function draw() {
   
+
+	var eeg = muse.get('/muse/eeg');
+	console.log('eeg');
+	console.log(eeg);
+
+	var raw_fft0 = muse.get('/muse//muse/elements/raw_fft0');
+	console.log('raw_fft0');
+	console.log(raw_fft0);
+
+
+	var mellow = muse.get('/muse/elements/experimental/mellow');
+	console.log('mellow');
+	console.log(mellow);
+
+
 }
+
+/*
+put this callback into museData in order to make things easier
 
 function parseMsg(msg) {
 		console.log('parseMsg',msg);
@@ -28,3 +49,4 @@ function parseMsg(msg) {
   
   
 }
+*/
